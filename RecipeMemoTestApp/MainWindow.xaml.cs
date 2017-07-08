8612c0d0a920fa8a2865090983e9f7b9c0df4a6e
reniris <nyoronu@gmail.com>
2017-07-09 00:16:29 +0900

    テスト用アプリプロジェクト追加

---
+++ RecipeMemoTestApp/MainWindow.xaml.cs
@@ -0,0 +1,34 @@
+﻿using Grabacr07.KanColleViewer.Composition;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace RecipeMemoTestApp
+{
+    /// <summary>
+    /// MainWindow.xaml の相互作用ロジック
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            var r = new RecipeMemo.RecipeMemo();
+            r.Initialize();
+
+            var t = (ITool)r;
+            this.Content = t.View;
+        }
+    }
+}
