f4b286f1774b6d6a26886882c93b251a15b88136
reniris <nyoronu@gmail.com>
2017-07-18 00:04:27 +0900

    スクロールしないのを修正

--- RecipeMemo/RecipeMemo.cs
+++ RecipeMemo/RecipeMemo.cs
@@ -18,7 +18,7 @@ namespace RecipeMemo
     [ExportMetadata("Guid", "42ED0309-9AE0-48C5-846A-1AE125678737")]
     [ExportMetadata("Title", "RecipeMemo")]
     [ExportMetadata("Description", "開発レシピを表示します")]
-    [ExportMetadata("Version", "2.1")]
+    [ExportMetadata("Version", "2.2")]
     [ExportMetadata("Author", "reniris")]
     public class RecipeMemo : IPlugin, ITool
     {
